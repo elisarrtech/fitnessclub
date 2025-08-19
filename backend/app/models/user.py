1a451883f27b28179a21ce7720eda8590d60936c
elisarrtech <146400511+elisarrtech@users.noreply.github.com>
2025-08-19 17:55:46 -0600

    Update user.py

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,9 +1,18 @@
 from pydantic import BaseModel
 from typing import Optional
+from enum import Enum
+from datetime import datetime
+
+class Role(str, Enum):
+    USER = "USER"
+    INSTRUCTOR = "INSTRUCTOR"
+    ADMIN = "ADMIN"
 
 class User(BaseModel):
-    id: str
+    id: Optional[str] = None
     email: str
     name: Optional[str] = None
     phone: Optional[str] = None
-    role: str = "USER"
+    role: Role = Role.USER
+    created_at: datetime = datetime.now()
+    updated_at: datetime = datetime.now()
