d96d6288d90e021fd1ac03c9429474c64d161958
elisarrtech <146400511+elisarrtech@users.noreply.github.com>
2025-08-20 05:29:44 -0600

    Create app.py

---
+++ backend/app.py
@@ -0,0 +1,7 @@
+# app.py (en el directorio raíz)
+from app.main import app
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=True)
