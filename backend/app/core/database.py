dfcc581a5b1e7ee295ec10c9ac5a58fcd602a68c
elisarrtech <146400511+elisarrtech@users.noreply.github.com>
2025-08-19 19:01:31 -0600

    Update database.py

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,4 +1,4 @@
-
+# app/core/database.py
 from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Conexión a MongoDB
-MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017/fitnessclub")
+MONGODB_URI = os.getenv("MONGODB_URI", "mongodb+srv://elisarrtech:R_zeHWhW9iAhYyM@cluster0.yjot3u0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 client = MongoClient(MONGODB_URI)
 db = client[os.getenv("DATABASE_NAME", "fitnessclubdb")]
 
@@ -15,3 +15,12 @@ users_collection = db.users
 classes_collection = db.classes
 instructors_collection = db.instructors
 bookings_collection = db.bookings
+schedules_collection = db.schedules
+
+# Índices recomendados
+users_collection.create_index("email", unique=True)
+classes_collection.create_index("title")
+instructors_collection.create_index("email", sparse=True)
+bookings_collection.create_index([("schedule_id", 1), ("user_id", 1)], unique=True)
+schedules_collection.create_index("class_id")
+schedules_collection.create_index("start_ts")
