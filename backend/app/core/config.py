77434ed9166c02586bef40ea25f417c11f872e10
elisarrtech <146400511+elisarrtech@users.noreply.github.com>
2025-08-19 18:14:24 -0600

    Update config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     VERSION: str = "1.0.0"
     API_V1_STR: str = "/api/v1"
     
-    MONGODB_URI: str = "mongodb+srv://elisarrtech:CunmKUAxHKep6SEA@cluster0.yjot3u0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    MONGODB_URI: str = "mongodb+srv://elisarrtech:R_zeHWhW9iAhYyM@cluster0.yjot3u0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     DATABASE_NAME: str = "elisarrtech"
     
     JWT_SECRET: str = "fitnessclub_jwt_secret_2025"
