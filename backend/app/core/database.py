c753ac95b81316d41505c7c38cd396cfd140410e
elisarrtech <146400511+elisarrtech@users.noreply.github.com>
2025-08-20 14:33:13 -0600

    Update database.py

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 try:
     # Usar la URL completa de MongoDB Atlas
-    MONGODB_URI = os.getenv("MONGODB_URI", "mongodb+srv://elisarrtech:R_zeHWhW9iAhYyM@cluster0.yjot3u0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    MONGODB_URI = os.getenv("MONGODB_URI", "mongodb+srv://fitnessclubdb:R_zeHWhW9iAhYyM@cluster0.yjot3u0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
     # Configuración MÍNIMA para evitar problemas en Railway
     client = MongoClient(
@@ -20,7 +20,7 @@ try:
     )
     
     # Obtener nombre de base de datos
-    db_name = os.getenv("DATABASE_NAME", "elisarrtech")
+    db_name = os.getenv("DATABASE_NAME", "fitnessclubdb")
     db = client[db_name]
     
     # Colecciones
