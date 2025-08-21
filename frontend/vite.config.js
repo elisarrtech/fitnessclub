294df31cec2c18220a3f4d972f5984f378f15fa3
elisarrtech <146400511+elisarrtech@users.noreply.github.com>
2025-08-20 20:55:43 -0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,11 @@
-// frontend/vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
-  },
-  build: {
-    outDir: 'dist'
+    host: true,
+    port: 5173
   }
-})
+});
